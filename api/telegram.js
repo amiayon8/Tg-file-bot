3b197438eb26df9cd91bc6881a2d037e70d24dc8
Sarker Ayon <2640c83ayonrumc@gmail.com>
2024-11-09 19:57:56 +0600

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -16,53 +16,14 @@ bot.onText(/\/help/, (msg) => {
   bot.sendMessage(chatId, 'How can I assist you?');
 });
 
-// Receive text messages
+// Listen for any incoming message
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  
-  // Check if message contains any file
-  if (msg.document) {
-    bot.sendMessage(chatId, 'Received your document!');
-  } else if (msg.photo) {
-    bot.sendMessage(chatId, 'Nice photo!');
-  } else if (msg.audio) {
-    bot.sendMessage(chatId, 'Nice audio!');
-  } else {
-    bot.sendMessage(chatId, 'I received your message: ' + msg.text);
-  }
+
+  // Convert the message object to a string and send it back
+  const messageDetails = JSON.stringify(msg, null, 2); // Pretty print with 2 spaces
+  bot.sendMessage(chatId, `Here are the full message details:\n\n${messageDetails}`);
 });
-
-// Get file information
-bot.on('document', (msg) => {
-  const chatId = msg.chat.id;
-  const fileId = msg.document.file_id;
-
-  bot.getFile(fileId).then((fileInfo) => {
-    bot.sendMessage(chatId, `File Info: ${JSON.stringify(fileInfo)}`);
-  }).catch((error) => {
-    bot.sendMessage(chatId, 'Error getting file info.');
-  });
-});
-
-// Send a message
-function sendMessage(chatId, text) {
-  bot.sendMessage(chatId, text);
-}
-
-// Send a file
-function sendFile(chatId, filePath) {
-  bot.sendDocument(chatId, filePath);
-}
-
-// Send a photo
-function sendPhoto(chatId, photoUrl) {
-  bot.sendPhoto(chatId, photoUrl);
-}
-
-// Send audio
-function sendAudio(chatId, audioUrl) {
-  bot.sendAudio(chatId, audioUrl);
-}
 
 // Handle unexpected errors
 bot.on('polling_error', (error) => {
